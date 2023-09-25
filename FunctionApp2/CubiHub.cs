72994641ebb941c939bc0ad19dbca83db3d0411c
Deep Patel <pateldeep691996@gmail.com>
2023-09-25 23:16:37 +0530

    Intial commint.

---
+++ FunctionApp2/CubiHub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+namespace FunctionApp2
+{
+    public class CubiHub : ServerlessHub
+    {
+        [FunctionName("negotiate")]
+        public SignalRConnectionInfo Negotiate([HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequest req)
+        {
+            return Negotiate(req);
+        }
+
+        [FunctionName(nameof(OnConnected))]
+        public async Task OnConnected([SignalRTrigger] InvocationContext invocationContext)
+        {
+            await Clients.All.SendAsync("OnConnected");
+        }
+
+        [FunctionName(nameof(Broadcast))]
+        public async Task Broadcast([SignalRTrigger] InvocationContext invocationContext, string message)
+        {
+            await Clients.All.SendCoreAsync("Broadcast", Array.Empty<object>());
+        }
+
+        public void OnDisconnected([SignalRTrigger] InvocationContext invocationContext)
+        {
+        }
+
+    }
+}
